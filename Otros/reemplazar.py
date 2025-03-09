f792cb73765546477c1253758db1041c99f21d2a
NicolasSotoPrg <sotosancheznicolas306@gmail.com>
2025-03-09 12:09:18 -0500

    Update reemplazar.py

--- Otros/reemplazar.py
+++ Otros/reemplazar.py
@@ -1,1 +1,10 @@
-a
+entrada = input()
+letra = input()
+n = len(entrada)
+salida = ""
+for i in range(0, n - 1 + 1, 1):
+    if entrada[i] == letra:
+        salida = salida + "&"
+    else:
+        salida = salida + entrada[i]
+print(salida)
