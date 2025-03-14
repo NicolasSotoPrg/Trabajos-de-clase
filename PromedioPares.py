3e38d7584dc004fa902a2028676055cadc3b6762
NicolasSotoPrg <sotosancheznicolas306@gmail.com>
2025-03-13 19:03:07 -0500

    Create PromedioPares.py

---
+++ PromedioPares.py
@@ -0,0 +1,11 @@
+n = int(input("Ingresa la cantidad de numeros: "))
+par = 0
+suma = 0
+promedio = 0
+for i in range(n):
+    m = int(input("Ingresa un numero: "))
+    if m%2 == 0:
+        suma = suma + m
+        par = par +1
+        promedio = suma // par
+print("El promedio de los numeros pares que ingreso es: ",promedio)
