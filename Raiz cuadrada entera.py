08d13caa8d8e88c63473314cac58c2f18629b325
NicolasSotoPrg <sotosancheznicolas306@gmail.com>
2025-03-14 20:14:42 -0500

    Create Raiz cuadrada entera.py

---
+++ Raiz cuadrada entera.py
@@ -0,0 +1,7 @@
+n = int(input("Ingrese un numero: "))
+m = n**0.5
+m2 = m*m
+if m2 == n:
+    print("El numero si tiene raiz cuadrada entera y es",m)
+else:
+    print("El numero no tiene raiz cuadrada entera")
